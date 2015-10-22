7a5ae0195f264ec1fabda62bc6c4ecb82aae71c5
YJ Park <yjpark@gmail.com>
2015-10-22 19:41:49 +0800

    add compile include

---
+++ csproj.py
@@ -0,0 +1,21 @@
+# -*- coding: utf8 -*-
+
+import silp
+
+import os
+import sys
+
+def compile_include(folder):
+    lines = []
+    root = os.path.abspath(folder)
+    silp.info('Adding files to compile include: %s' % root)
+    files = [os.path.join(dirpath, f)
+             for dirpath, dirnames, files in os.walk(root)
+             for f in files if f.endswith(".cs")]
+    for path in files:
+        path = path.replace(root, '')
+        path = path.replace('/', '\\')
+        line = '<Compile Include="%s%s" />' % (folder, path)
+        lines.append(line)
+    return lines
+
