a2fab537f0a44502a863b9c83f511ee5a7ab4e87
YJ Park <yjpark@gmail.com>
2016-10-12 19:30:02 +0800

    handle xaml dependupon

--- projitems.py
+++ projitems.py
@@ -14,9 +14,34 @@ def compile_include(folder):
              for f in files if f.endswith(".cs")]
     folder = folder.replace('/', '\\')
     for path in files:
+        name = os.path.basename(path)
         path = path.replace(root, '')
         path = path.replace('/', '\\')
-        line = '<Compile Include="$(MSBuildThisFileDirectory)%s%s" />' % (folder, path)
+        if name.endswith('.xaml.cs'):
+            line = '<Compile Include="$(MSBuildThisFileDirectory)%s%s">' % (folder, path)
+            lines.append(line)
+            line = '  <DependentUpon>%s</DependentUpon>' % name.replace('.cs', '')
+            lines.append(line)
+            line = '</Compile>'
+            lines.append(line)
+        else:
+            line = '<Compile Include="$(MSBuildThisFileDirectory)%s%s" />' % (folder, path)
+            lines.append(line)
+    return lines
+
+
+def xaml_include(folder):
+    lines = []
+    root = os.path.abspath(folder)
+    silp.term.info('Adding files to embedded resource include: %s' % root)
+    files = [os.path.join(dirpath, f)
+             for dirpath, dirnames, files in os.walk(root)
+             for f in files if f.endswith(".xaml")]
+    folder = folder.replace('/', '\\')
+    for path in files:
+        path = path.replace(root, '')
+        path = path.replace('/', '\\')
+        line = '<EmbeddedResource Include="$(MSBuildThisFileDirectory)%s%s" />' % (folder, path)
         lines.append(line)
     return lines
 
